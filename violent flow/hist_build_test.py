5183ace3c67627a1a77e37f42e46ac31e355d048
sekharkaredla <sekhar.karedla@gmail.com>
2018-01-06 11:02:39 +0530

    feature vector

--- violent flow/hist_build_test.py
+++ violent flow/hist_build_test.py
@@ -8,8 +8,8 @@ flow_video = getViolentFlow('vio_1.avi')
 height = flow_video.shape[0]
 width = flow_video.shape[1]
 
-B_height = math.floor((height - 11)/4)
-B_width = math.floor((width - 11)/4)
+B_height = int(math.floor((height - 11)/4))
+B_width = int(math.floor((width - 11)/4))
 
 def histc(X, bins):
     map_to_bins = np.digitize(X,bins)
@@ -20,9 +20,22 @@ def histc(X, bins):
 
 bins = np.arange(0.0,1.05,0.05,dtype=np.float64)
 
+
 def getBlockHist(flow_video):
     flow_vec = np.reshape(flow_video,(flow_video.shape[0]*flow_video.shape[1],1))
     count_of_bins = histc(flow_vec,bins)
     return count_of_bins/np.sum(count_of_bins)
 
-print getBlockHist(flow_video)
+def getFeatureVector():
+    frame_hist = []
+    for y in range(6,height-B_height-5,B_height):
+        for x in range(6,width-B_width-5,B_width):
+            block_hist = getBlockHist(flow_video[y:y+B_height-1,x:x+B_width-1])
+            if(len(frame_hist)!=0):
+                frame_hist = np.append(frame_hist,block_hist,axis = 0)
+            else:
+                frame_hist = block_hist
+    return frame_hist
+
+
+getFeatureVector()
