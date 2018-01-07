446ae9be6ea26ae54441af044392016553557d74
sekhar <sekhar.karedla@gmail.com>
2018-01-08 00:54:30 +0530

    cleaned files

--- Constants.py
+++
@@ -1,6 +0,0 @@
-FRAME_RATE = 25 #25 frames per second
-MOVEMENT_INTERVAL = 3 #difference between considered frames
-N = 4 #number of vertical blocks per frame
-M = 4 #number of horizontal blocks per frame
-
-K = 4
