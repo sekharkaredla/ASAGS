25d53be6bd832e1102aa1a4fa2a9e4e332b5287c
sekharkaredla <sekhar.karedla@gmail.com>
2018-01-10 22:48:23 +0530

    dont commit with change of path

--- ViolentFlowFeature/violent_features_VIOLENT.py
+++ ViolentFlowFeature/violent_features_VIOLENT.py
@@ -1,10 +1,11 @@
 from ViolentFlow import VioFlow
 file_vio = open('violent_list.txt')
-path = '/home/dasarada/Desktop/Violence/'
+path = '/Users/roshni/Desktop/VideoData/Violence/'
 for each_file in file_vio.readlines():
     each_file = each_file[:-1]
     feature = VioFlow(path + each_file)
     out_file = each_file[:-3] + 'txt'
+    print each_file + '-----------------------------------------------------'
     try:
         feature.writeFeatureToFile('violent_features_VIOLENT/' + out_file)
         print each_file + '  done'
