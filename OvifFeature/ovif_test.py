cedd3439b938345d065abfae5d8ce8c5090db32a
sekhar <sekhar.karedla@gmail.com>
2018-01-20 01:33:54 +0530

    time measured

--- OvifFeature/ovif_test.py
+++ OvifFeature/ovif_test.py
@@ -1,4 +1,7 @@
+import time
 from Ovif import OvifCalc
-obj = OvifCalc('vio_1.avi')
+obj = OvifCalc('input1.avi')
+start_time = time.time()
 feature = obj.getOvifFeature()
 print feature,feature.shape
+print("--- %s seconds ---" % (time.time() - start_time))
