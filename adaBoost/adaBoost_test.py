48f35db03939d7eb2f5b1a49fef7f28ae028a4f1
sekhar <sekhar.karedla@gmail.com>
2018-03-05 02:54:05 +0530

    --

--- adaBoost/adaBoost_test.py
+++ adaBoost/adaBoost_test.py
@@ -73,9 +73,7 @@ for i in data_violent[81:]:
         print 'error in reading nonvio_%d.txt'%i
 
 # train contains the training set and test contains the testing set
-
 classifiers = {}
-w = [1.0/(2*129)]*252
 for each_feature in range(0,252):
     # generating classifier for each feature
     train_data_X = []
@@ -90,8 +88,56 @@ for each_feature in range(0,252):
     classifiers[each_feature] = clf
 
 #calculate error for each classifier
-errors = [0.0]*252
 
-for each_feature in range(0,252):
+w = np.array([float(1)/(2*129)]*252)
+for t in range(0,10): 
+    #w = [x/sum(w) for x in w]
+    #w = list(np.array(w,dtype = np.float64)/sum(np.array(w,dtype = np.float64)))
+    # normalize weights
+    # sumw = np.array([np.log10(sum(w))]*252)
+    # logw = np.log10(w)
+    # print logw
+    # logw = logw - sumw
+    # w = [float(10.0)**x for x in logw]   
+    w = w/np.sum(w)
+    errors = [0.0]*252
+    for each_feature in range(0,252):
+        clf = classifiers[each_feature]
+        i = 0
+        preds = []
+        for each_set in X_test:
+            test_data_X = each_set[each_feature]
+            test_data_Y = Y_test[i]
+            i += 1
+            preds.append(clf.predict(test_data_X.reshape(1,-1))[0])
+        i = 0
+        for each_pred in preds:
+            errors[each_feature] += w[each_feature] * abs(each_pred-Y_test[i])
+            i += 1
     
+
+    min_error_classifier_index = np.where(errors == np.amin(errors))
+
     
+    min_error_classifier_index = min_error_classifier_index[0][0]
+    # print min_error_classifier_index
+    print errors , errors[min_error_classifier_index]
+    beta_change = errors[min_error_classifier_index]/(float(1) - errors[min_error_classifier_index])
+    print beta_change
+    # print min_error_classifier_index
+    # calculate predictions for further use
+    for each_feature in range(0,252):
+        clf = classifiers[each_feature]
+        i = 0
+        preds = []
+        for each_set in X_test:
+            test_data_X = each_set[each_feature]
+            test_data_Y = Y_test[i]
+            i += 1
+            preds.append(clf.predict(test_data_X.reshape(1,-1))[0])
+        i = 0
+    # update weights
+        for each_set in X_test:
+            w[each_feature] = w[each_feature] * (beta_change ** (1-(abs(preds[i]-Y_test[i]))))
+            i += 1
+
