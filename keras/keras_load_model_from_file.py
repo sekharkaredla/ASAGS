9a97711fba719803ac4a143d00731678058577d9
dasarada <dachi8958@gmail.com>
2018-03-14 20:54:02 +0530

    model visualized

--- keras/keras_load_model_from_file.py
+++ keras/keras_load_model_from_file.py
@@ -1,5 +1,6 @@
 from keras.models import model_from_json
-from keras.utils.vis_utils import plot_model
+from keras.utils.vis_utils import plot_model, model_to_dot
+from IPython.display import SVG,display_svg
 
 # load model
 json_file = open('model_100.json', 'r')
@@ -11,5 +12,7 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("model_100.h5")
 
 plot_model(loaded_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+display(SVG(model_to_dot(loaded_model).create(prog='dot', format='svg')))
 
 print("Loaded model from disk")
