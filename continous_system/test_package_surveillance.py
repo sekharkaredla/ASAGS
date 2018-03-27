b06dc978f48bec25a68b7ef43feec6f3e6eb8cb1
sekharkaredla <sekhar.karedla@gmail.com>
2018-03-27 12:44:52 +0530

    main file

--- continous_system/test_package_surveillance.py
+++ continous_system/test_package_surveillance.py
@@ -1,5 +1,6 @@
 from ContSurv import ContinousSurv
-obj = ContinousSurv()
-# obj.setVideoName('testV2.avi')
-# obj.doSurveillanceFromVideo()
-obj.doSurveillanceFromCamera()
+if __name__ == '__main__':
+	obj = ContinousSurv()
+	obj.setVideoName('testV2.avi')
+	obj.doSurveillanceFromVideo()
+	# obj.doSurveillanceFromCamera()
