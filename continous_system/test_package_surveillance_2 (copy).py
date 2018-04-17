db6c861c7dac236d153edc34b06bf270d3b5445a
dasarada <dachi8958@gmail.com>
2018-04-17 14:13:11 +0530

    new videos

---
+++ continous_system/test_package_surveillance_2 (copy).py
@@ -0,0 +1,10 @@
+import time
+from ContSurv import ContinousSurv2
+if __name__ == '__main__':
+    start_time = time.time()
+    obj = ContinousSurv2()
+    obj.setVideoName('nepal2.avi')
+    obj.doSurveillanceFromVideo()
+    # obj.doSurveillanceFromCamera()
+    print '-----------------------------'
+    print 'total time taken : ' + str(start_time - time.time())
